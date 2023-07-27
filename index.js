920903b93fc7d2038123e4f7f9757ff920519e10
Ameen Ul Islam <ameenulislam4@gmail.com>
2023-07-27 21:14:35 +0530

    connected mongo via cyclic way

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const app = express();
 const password = encodeURIComponent('eIPdm07tnXKxL3td'); 
 const uri = 'mongodb+srv://ameenulislam4:eIPdm07tnXKxL3td@zephmailer.pycpj9a.mongodb.net/?retryWrites=true&w=majority';
 
-const baseUrl = '/.netlify/functions/';
+const PORT = process.env.PORT || 3000
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB:', err));
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 app.use(express.json());
 
@@ -22,4 +31,8 @@ app.use('/users', userRouter);
 const createUserRouter = require('./routes/createUser')
 app.use('/addUser', createUserRouter);
 
-app.listen(3000, ()=> console.log('server started'));
+connectDB().then(() => {
+  app.listen(PORT, () => {
+      console.log("listening for requests");
+  })
+})
