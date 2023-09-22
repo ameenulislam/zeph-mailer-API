a59cc56aac0fb642972f7c49c1df7be3064b3680
Ameen Ul Islam <ameenulislam4@gmail.com>
2023-09-22 21:42:43 +0530

    Cors removed

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const cors = require('cors');
-
 
 const app = express();
 
 
 require('dotenv').config();
 
-app.use(cors({
-  origin: ['https://zephmailer.netlify.app', '*']
-}));
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const PORT = process.env.PORT || 3000
 const connectDB = async () => {
